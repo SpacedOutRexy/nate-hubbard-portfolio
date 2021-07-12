afdd687d46682e7eda74e2f7834d445ab2461829
Code Success <codesuccess@cwi.edu>
2021-07-12 08:42:37 -0600

    Second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Nathaniel Hubbard Portfolio</h1>
-        <div>
-          {moment().format('MMM Do YYY, h:mm:ss a')}
-        </div>
+        <PortfolioContainer />
+        <div>{moment().format('MMM Do YYY, h:mm:ss a')}</div>
       </div>
     );
   }
